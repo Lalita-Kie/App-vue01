b5c7f5108b099585615f9279e0e7008d66b6e4d4
Lalita-Kie <Lalitakamyuta@gmail.com>
2026-01-11 18:31:44 +0700

    update v2

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,37 @@
 <template>
-    <div>
+    <div class="text-primary">
         <h1>Contact to Lalita Kamyuta</h1>
         <h3>Tel.0988292107</h3>
-        <img alt="Vue logo" src="../assets/cat.jpg">
-    </div>
+        <img alt="Vue logo" width="50%" src="../assets/cat.jpg">
+
+        <br><br><br>
+
+
+<div class="container">
+    <div class="row">
+    <!-- 1 -->
+    <div class="col-sm-6">
+         <img alt="Vue logo" width="90%" src="../assets/cat.jpg">
+      </div>
+    <!-- 2 -->
+    <div class="col-sm-6 bg-body-tertiary">
+    <div class="mb-3 mt-4">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Detail</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+ <div class="mb-3 textcenter">
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  
+  </div>
+
+</div>
+  </div>
+
+</div>
+</div>
 </template>
-
-
